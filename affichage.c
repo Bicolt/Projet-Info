47202023c9e434e0766694ac5b0f674b96982060
Nicolas Benning <nicolas-benning@telecom-sudparis.eu>
2013-04-09 00:05:48 +0200

    New Fonction afficherRectangle

--- affichage.c
+++ affichage.c
@@ -15,3 +15,17 @@ void afficherTexte (SDL_Surface *ecran, char *font, int size, char *txt, int x, 
     SDL_BlitSurface(texte, NULL, ecran, &positiontexte);
     TTF_CloseFont(police);
 }
+
+void afficherRectangle (SDL_Surface *ecran, int largeur, int hauteur, int posx, int posy, int alpha) {
+    SDL_Surface *rectangle = NULL;
+    SDL_Rect position;
+
+    rectangle = SDL_CreateRGBSurface(SDL_HWSURFACE, largeur, hauteur, 32, 0, 0, 0, 0);
+    SDL_FillRect(rectangle, NULL, SDL_MapRGB(ecran->format, 0, 0, 0));
+    SDL_SetAlpha(rectangle, SDL_SRCALPHA, alpha);
+    SDL_SetColorKey(rectangle, SDL_SRCCOLORKEY, SDL_MapRGB(rectangle->format, 255, 255, 255));
+
+    position.x = posx;
+    position.y = posy;
+    SDL_BlitSurface(rectangle, NULL, ecran, &position);
+}
--- affichage.h
+++ affichage.h
@@ -1,1 +1,2 @@
 void afficherTexte (SDL_Surface *ecran, char *font, int size, char *txt, int x, int y);
+void afficherRectangle (SDL_Surface *ecran, int largeur, int hauteur, int posx, int posy, int alpha);
