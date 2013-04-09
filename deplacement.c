10c737494da6af63504b9049e84b66fcbc7a0bb2
Thomas <thomas.laurence@telecom-sudparis.eu>
2013-04-09 16:17:15 +0200

    remodif

--- deplacement.c
+++ deplacement.c
@@ -10,13 +10,13 @@
 #include "affichage.h"
 
 unsigned int getpixel(SDL_Surface *s, int x, int y);
-int avancer ( SDL_Rect* pposperso, SDL_Surface* ecran);
-int solsouspieds ( SDL_Rect* pposperso, SDL_Surface* ecran);
-void tomber ( SDL_Rect* pposperso, SDL_Surface* ecran);
-int monter( SDL_Rect* pposperso, SDL_Surface* ecran );
-int plater( SDL_Rect* pposperso, SDL_Surface* ecran );
-int descendre( SDL_Rect* pposperso, SDL_Surface* ecran );
-int sortir (SDL_Rect *pposperso, SDL_Surface *ecran);
+int avancer ( SDL_Rect* pposperso, SDL_Surface* terrain);
+int solsouspieds ( SDL_Rect* pposperso, SDL_Surface* terrain);
+void tomber ( SDL_Rect* pposperso, SDL_Surface* terrain);
+int monter( SDL_Rect* pposperso, SDL_Surface* terrain );
+int plater( SDL_Rect* pposperso, SDL_Surface* terrain );
+int descendre( SDL_Rect* pposperso, SDL_Surface* terrain );
+int sortir (SDL_Rect *pposperso, SDL_Surface *terrain);
 
 
 unsigned int getpixel(SDL_Surface *s, int x, int y) {
@@ -24,38 +24,38 @@ unsigned int getpixel(SDL_Surface *s, int x, int y) {
     return ((unsigned int*)s->pixels)[y*(s->pitch/sizeof(unsigned int)) + x];
 }
 
-int avancer ( SDL_Rect* pposperso, SDL_Surface* ecran) {
-    if (sortir(pposperso, ecran)){
-        if(pause(ecran))
+int avancer ( SDL_Rect* pposperso, SDL_Surface* terrain) {
+    if (sortir(pposperso, terrain)){
+        if(pause(terrain))
             return 0;
          }
-    if (solsouspieds(pposperso, ecran) == 0) {
-        tomber(pposperso, ecran);
+    if (solsouspieds(pposperso, terrain) == 0) {
+        tomber(pposperso, terrain);
         return 2;
     }
-    if (monter(pposperso, ecran) == 1) {
-        tomber(pposperso, ecran);
-        tomber(pposperso, ecran);
+    if (monter(pposperso, terrain) == 1) {
+        tomber(pposperso, terrain);
+        tomber(pposperso, terrain);
     }
     else {
-        if ( plater(pposperso, ecran) == 1 ) {
-            tomber(pposperso, ecran);
+        if ( plater(pposperso, terrain) == 1 ) {
+            tomber(pposperso, terrain);
         }
         else {
-            descendre(pposperso, ecran);
+            descendre(pposperso, terrain);
         }
     }
     return 1;
 }
 
-int monter( SDL_Rect* pposperso, SDL_Surface* ecran ) {
+int monter( SDL_Rect* pposperso, SDL_Surface* terrain ) {
     int i;
     for(i=0 ; i<= 10 ; i++) {
-        if ( getpixel(ecran, pposperso->x + 5*i + 4, pposperso->y - 5) == 4278190080LL )
+        if ( getpixel(terrain, pposperso->x + 5*i + 4, pposperso->y - 5) == 4278190080LL )
             return 0;
     }
     for(i=0 ; i<= 20 ; i++) {
-        if ( getpixel(ecran, pposperso->x + 64, pposperso->y + 5*i - 5) == 4278190080LL )
+        if ( getpixel(terrain, pposperso->x + 64, pposperso->y + 5*i - 5) == 4278190080LL )
             return 0;
     }
     //Si on a survécu aux deux boucles, on peut monter (meme si on est sur du plat). Du coup on s'en prive pas.
@@ -64,14 +64,14 @@ int monter( SDL_Rect* pposperso, SDL_Surface* ecran ) {
     return 1;
 }
 
-int descendre( SDL_Rect* pposperso, SDL_Surface* ecran ) {
+int descendre( SDL_Rect* pposperso, SDL_Surface* terrain ) {
     int i;
     for(i=0 ; i<= 10 ; i++) {
-        if ( getpixel(ecran, pposperso->x + 5*i + 4, pposperso->y + 145) == 4278190080LL )
+        if ( getpixel(terrain, pposperso->x + 5*i + 4, pposperso->y + 145) == 4278190080LL )
             return 0;
     }
     for(i=0 ; i<= 20 ; i++) {
-        if ( getpixel(ecran, pposperso->x + 64, pposperso->y + 5 + 5*i) == 4278190080LL )
+        if ( getpixel(terrain, pposperso->x + 64, pposperso->y + 5 + 5*i) == 4278190080LL )
             return 0;
     }
     //Si on a survécu aux deux boucles, on peut avancer. Du coup on s'en prive pas.
@@ -80,39 +80,39 @@ int descendre( SDL_Rect* pposperso, SDL_Surface* ecran ) {
     return 1;
 }
 
-int plater( SDL_Rect* pposperso, SDL_Surface* ecran ) {
+int plater( SDL_Rect* pposperso, SDL_Surface* terrain ) {
     int i;
     for(i=0 ; i< 20 ; i++) {
-        if ( getpixel(ecran, pposperso->x + 64, pposperso->y + 5*i) == 4278190080LL )
+        if ( getpixel(terrain, pposperso->x + 64, pposperso->y + 5*i) == 4278190080LL )
             return 0;
     }
-    if ( getpixel(ecran, pposperso->x + 64, pposperso->y + 139) == 4278190080LL ) //On teste le pixel tout en bas à par, pour le réhausser d'un pixel.
+    if ( getpixel(terrain, pposperso->x + 64, pposperso->y + 139) == 4278190080LL ) //On teste le pixel tout en bas à par, pour le réhausser d'un pixel.
         return 0;
     pposperso->x += 4;
     return 1;
 }
 
-int solsouspieds ( SDL_Rect* pposperso, SDL_Surface* ecran) {
+int solsouspieds ( SDL_Rect* pposperso, SDL_Surface* terrain) {
     int i;
     for(i=0 ; i<= 10 ; i++) {
-        if ( getpixel(ecran, pposperso->x - 5*i + 60, pposperso->y + 140) == 4278190080LL )
+        if ( getpixel(terrain, pposperso->x - 5*i + 60, pposperso->y + 140) == 4278190080LL )
             return 1;
     }
     return 0;
 
 }
 
-void tomber ( SDL_Rect* pposperso, SDL_Surface* ecran) {
+void tomber ( SDL_Rect* pposperso, SDL_Surface* terrain) {
     int i;
     for(i = 0 ; i < 5 ; i++) {
-        if (solsouspieds(pposperso, ecran) == 1)
+        if (solsouspieds(pposperso, terrain) == 1)
             break;
         (pposperso->y) ++ ;
     }
 }
 
-int sortir (SDL_Rect *pposperso, SDL_Surface *ecran){
-    if (pposperso->y > ecran->h){
+int sortir (SDL_Rect *pposperso, SDL_Surface *terrain){
+    if (pposperso->y > terrain->h){
         return 1;
     }
     if (pposperso->x < 0){
