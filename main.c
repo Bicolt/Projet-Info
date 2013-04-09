2e8ea682f36213e571d641ae528874bfc0752900
Bilge Kimyonok <bilge.kimyonok@telecom-sudparis.eu>
2013-04-09 17:14:19 +0200

    Ajout du Game Over
    GAME OVER !!!

--- main.c
+++ main.c
@@ -9,6 +9,11 @@
 #include "niveau.h"
 
 SDL_Surface* selection(int largeur, int hauteur, SDL_PixelFormat *pf);
+int gameover(SDL_Surface *ecran);
+void menu(SDL_Surface *ecran, TTF_Font *police, int *choix);
+int pause(SDL_Surface* ecran);
+int choixNiveau (SDL_Surface *ecran);
+
 
 int main(int argc, char *argv[]){
 
@@ -492,4 +497,135 @@ int choixNiveau (SDL_Surface *ecran) {
     SDL_Flip(ecran);
     }
     return 0;
+}
+
+int gameover(SDL_Surface *ecran){ // Devra prendre en entrée plus tard le niveau chargé pour le recharger dans recommencer
+
+    TTF_Font *police = TTF_OpenFont("ariblk.ttf", 55);
+    SDL_Surface *titre = NULL, *texte1 =NULL, *texte2 = NULL, *texte3 = NULL, *rect = NULL, *fond = NULL;
+	SDL_Color Black = {0, 0, 0};
+    SDL_Event event;
+    SDL_Rect positiontitre, positiontexte1, positiontexte2, positiontexte3, posrec, pos;
+    int xSouris, ySouris;
+    int continuer = 1, select=0;
+
+    pos.x = 0; pos.y = 0;
+
+    rect = selection(ecran->w, 80, ecran->format);
+    titre = TTF_RenderText_Blended(police, "GAME OVER", Black);
+    texte1 = TTF_RenderText_Blended(police, "Recommencer", Black);
+    texte2 = TTF_RenderText_Blended(police, "Menu principal", Black);
+    texte3 = TTF_RenderText_Blended(police, "Quitter", Black);
+	fond = SDL_CreateRGBSurface(SDL_HWSURFACE, ecran->w, ecran->h, 32, 0, 0, 0, 0);
+	SDL_FillRect(fond, NULL, SDL_MapRGB(ecran->format, 255, 255, 255));
+    // fond = IMG_Load("ressources/1.jpg"); lorsque le fond est une image
+
+    positiontitre.x = ((ecran->w - titre->w)/2);
+    positiontitre.y = (ecran->h/2 - 300);
+    positiontexte1.x = ((ecran->w - texte1->w)/2);
+    positiontexte2.x = ((ecran->w - texte2->w)/2);
+	positiontexte3.x = ((ecran->w - texte3->w)/2);
+    positiontexte1.y = (ecran->h/2 - 175);
+    positiontexte2.y = (ecran->h/2 + 15);
+    positiontexte3.y = (ecran->h/2 + 205);
+    posrec.x = 0;
+    posrec.y = positiontexte1.y + 5;
+
+    SDL_BlitSurface(fond, NULL, ecran, &pos);
+    SDL_BlitSurface(titre, NULL, ecran, &positiontitre);
+    SDL_BlitSurface(texte1, NULL, ecran, &positiontexte1);
+    SDL_BlitSurface(texte2, NULL, ecran, &positiontexte2);
+	SDL_BlitSurface(texte3, NULL, ecran, &positiontexte3);
+    SDL_BlitSurface(rect, NULL, ecran, &posrec);
+    SDL_Flip(ecran);
+
+    SDL_EnableKeyRepeat(150, 10);
+
+   while(continuer){
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_MOUSEMOTION:
+                xSouris = event.button.x;
+                ySouris = event.button.y;
+                if(((ecran->h/2 - 175) <= ySouris) && (ySouris < (ecran->h/2 + 15))){
+                    select = 0;
+                    posrec.y=ecran->h/2 - 175 + 5;
+                }
+                else if(((ecran->h/2 + 15) <= ySouris) && (ySouris < (ecran->h/2 + 205))){
+                    select = 1;
+                    posrec.y=ecran->h/2 +15 +5;
+                }
+                else if(((ecran->h/2 + 205) <= ySouris) && (ySouris < (ecran->h/2 + 395))){
+                    select = 2;
+                    posrec.y=ecran->h/2 + 205 +5;
+                }
+                break;
+            case SDL_MOUSEBUTTONUP:
+                if(event.button.button == SDL_BUTTON_LEFT){
+                    xSouris = event.button.x;
+                    ySouris = event.button.y;
+                    if(((ecran->h/2 - 175) <= ySouris) && (ySouris < (ecran->h/2 + 15))){
+                        niveau(ecran);
+                      return 1;
+                    }
+                    else if(((ecran->h/2 + 15) <= ySouris) && (ySouris < (ecran->h/2 + 205))){
+                        return 1;
+                    }
+                    else if(((ecran->h/2 + 205) <= ySouris) && (ySouris < (ecran->h/2 + 395))){
+                     SDL_Quit();
+                        continuer = 0;
+                    }
+                }
+            break;
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym)
+                {
+                    case SDLK_UP:
+                        select = (select + 2)%3;
+                        if(select == 2)
+                            posrec.y = posrec.y + 380;
+                        else posrec.y = posrec.y - 190;
+                        break;
+                    case SDLK_DOWN:
+                        select = (select + 1)%3;
+                        if(select == 0)
+                            posrec.y = posrec.y - 380;
+                        else posrec.y = posrec.y + 190;
+                        break;
+                    case SDLK_ESCAPE:
+                            SDL_Quit();
+                            continuer = 0;
+                        break;
+                    case SDLK_RETURN:
+                        if(select == 2){
+                                  SDL_Quit();
+                            continuer = 0;
+                        }
+                        else if(select == 1){
+                            return 1;
+                        }
+                        else{
+                            niveau(ecran);
+                            return 1;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case SDL_QUIT:
+                    continuer = 0;
+                break;
+        }
+    SDL_Delay(30);
+    SDL_BlitSurface(fond, NULL, ecran, &pos);
+    SDL_BlitSurface(titre, NULL, ecran, &positiontitre);
+    SDL_BlitSurface(texte1, NULL, ecran, &positiontexte1);
+    SDL_BlitSurface(texte2, NULL, ecran, &positiontexte2);
+	SDL_BlitSurface(texte3, NULL, ecran, &positiontexte3);
+    SDL_BlitSurface(rect, NULL, ecran, &posrec);
+    SDL_Flip(ecran);
+    }
+    return 1;
 }
--- main.h
+++ main.h
@@ -2,3 +2,4 @@ void menu(SDL_Surface *ecran, TTF_Font *police, int *choix);
 int pause(SDL_Surface* ecran);
 SDL_Surface* selection(int largeur, int hauteur, SDL_PixelFormat *pf);
 int choixNiveau (SDL_Surface *ecran);
+int gameover(SDL_Surface *ecran);
