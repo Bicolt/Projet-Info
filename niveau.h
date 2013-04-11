d54f754b316cc5bc4e0e464b5407d10d7ba4fb34
Thomas <thomas.laurence@telecom-sudparis.eu>
2013-04-11 15:36:00 +0200

    mise a jour de niveau.h

--- niveau.h
+++ niveau.h
@@ -7,7 +7,8 @@
 int min(int a, int b);
 int max(int a, int b);
 void pointilleSelection(SDL_Surface *surfSelec, SDL_Rect selecNiveau, SDL_Rect pos, int xSouris, int ySouris);
-void decouperColler(cairo_surface_t *surfaceFond, SDL_Rect posSelection, SDL_Rect posDestination);
+void recollementContinu(SDL_Surface *surfLigne, SDL_Rect posSelection, SDL_Rect *posDestination);
+void decouperColler(SDL_Surface * surfLigne, cairo_surface_t *surfaceFond, SDL_Rect posSelection, SDL_Rect posDestination);
 int niveau(SDL_Surface *ecran, int choixTerrain);
 void chargerTerrain(SDL_Surface *ecran, cairo_surface_t *surfaceFond, int choixTerrain);
 cairo_t * pperso(SDL_Surface *ecran, SDL_Surface * surfPerso);
