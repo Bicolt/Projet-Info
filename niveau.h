855f0151aa4d6d803a3aa29bf83a8cbb3ad60250
Bilge Kimyonok <bilge.kimyonok@telecom-sudparis.eu>
2013-04-16 17:56:20 +0200

    Ajout d'un fichier terrains.c
    Pour simplifier le travail sur niveau et terrain en simultané.

--- niveau.h
+++ niveau.h
@@ -12,10 +12,3 @@ void decouperColler(SDL_Surface * surfLigne, cairo_surface_t *surfaceFond, SDL_R
 int niveau(SDL_Surface *ecran, int choixTerrain);
 void chargerTerrain(SDL_Surface *ecran, cairo_surface_t *surfaceFond, int choixTerrain);
 cairo_t * pperso(SDL_Surface *ecran, SDL_Surface * surfPerso);
-void tterrain0(SDL_Surface *ecran, cairo_surface_t * surfaceFond);
-void tterrain1(SDL_Surface *ecran, cairo_surface_t * surfaceFond);
-void tterrain2(SDL_Surface *ecran, cairo_surface_t * surfaceFond);
-void tterrain3(SDL_Surface *ecran, cairo_surface_t * surfaceFond);
-void tterrain4(SDL_Surface *ecran, cairo_surface_t * surfaceFond);
-void tterrain5(SDL_Surface *ecran, cairo_surface_t * surfaceFond);
-void tterrain6(SDL_Surface *ecran, cairo_surface_t * surfaceFond);
---
+++ terrains.h
@@ -0,0 +1,8 @@
+void tterrain0(SDL_Surface *ecran, cairo_surface_t * surfaceFond);
+void tterrain1(SDL_Surface *ecran, cairo_surface_t * surfaceFond);
+void tterrain2(SDL_Surface *ecran, cairo_surface_t * surfaceFond);
+void tterrain3(SDL_Surface *ecran, cairo_surface_t * surfaceFond);
+void tterrain4(SDL_Surface *ecran, cairo_surface_t * surfaceFond);
+void tterrain5(SDL_Surface *ecran, cairo_surface_t * surfaceFond);
+void tterrain6(SDL_Surface *ecran, cairo_surface_t * surfaceFond);
+
